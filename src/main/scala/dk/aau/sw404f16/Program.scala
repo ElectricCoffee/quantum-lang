5577e7e28147e75b7147bfc9d1a288611d1c9041
electric coffee <slench102@gmail.com>
2016-03-28 21:59:01 +0200

    add tests
    * moves the tests from the main methods to the src/test/scala folder
    + project now uses ScalaTest for testing
    + adds three tests, two for tokenizeString, and one for createTokenList

--- src/main/scala/dk/aau/sw404f16/Program.scala
+++ src/main/scala/dk/aau/sw404f16/Program.scala
@@ -7,9 +7,6 @@ import dk.aau.sw404f16.lexer.Lexer
   */
 object Program {
   def main(args: Array[String]) = {
-    val lst = List("for", "if", "then", "hello", "=", "actor", "(", "{", "[", "0xbadf00d", ">>=",
-      "0b1101111010101101", "#pop-dat-booty", "\"welcome to \\\"Homestarrunner.com\\\"\"")
-
-    lst.map(Lexer.tokenizeString).foreach(println)
+    // look in src/test/scala for the tests
   }
 }
---
+++ src/test/scala/TokenizerSpec.scala
@@ -0,0 +1,43 @@
+import dk.aau.sw404f16.lexer._
+import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException
+import org.scalatest._
+/**
+  * Created by coffee on 28/03/16.
+  */
+class TokenizerSpec extends FlatSpec with Matchers {
+  val addExample = List(Function, Identifier("add"), LeftParen,
+    Identifier("Num"), Identifier("x"), Comma, Identifier("Num"), Identifier("y"), RightParen, Assignment,
+    Identifier("x"), Operator("+"), Identifier("y"), Semicolon)
+
+  val tellExample = List(Tell, Identifier("stack"), About, Atom("#push"),
+    LeftParen, StringLiteral("\"the man said \\\"hello\\\"\""), RightParen, Semicolon)
+
+  "The Lexer" should "translate string inputs to objects of the Token trait" in {
+    val test1 = List("tell", "stack", "about", "#push", "(", "\"the man said \\\"hello\\\"\"", ")", ";")
+    val test2 = List("func", "add", "(", "Num", "x", ",", "Num", "y", ")", "=", "x", "+", "y", ";")
+    val test3 = "0b1101111010101101"
+    val test4 = "0xbadf00d"
+
+    test1.map(Lexer.tokenizeString) should be (tellExample)
+
+    test2.map(Lexer.tokenizeString) should be (addExample)
+
+    Lexer.tokenizeString(test3) should be (BinaryLiteral("0b1101111010101101"))
+
+    Lexer.tokenizeString(test4) should be (HexLiteral("0xbadf00d"))
+  }
+
+  it should "throw SyntaxException if fed a string it doesn't recognise" in {
+    val garbage = "sdflk#$!#SD!#"
+
+    a [SyntaxException] should be thrownBy {
+      Lexer.tokenizeString(garbage)
+    }
+  }
+
+  it should "parse an uninterrupted string into a list of tokens" in {
+    val codeString = "func add(Num x, Num y) = x + y;"
+
+    Lexer.createTokenList(codeString) should be (addExample)
+  }
+}
