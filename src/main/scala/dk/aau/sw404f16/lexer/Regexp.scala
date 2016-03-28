3109b6c2926a6283d5f03a4671aab203140eb619
electric coffee <slench102@gmail.com>
2016-03-28 21:56:17 +0200

    fix regexp errors

--- src/main/scala/dk/aau/sw404f16/lexer/Regexp.scala
+++ src/main/scala/dk/aau/sw404f16/lexer/Regexp.scala
@@ -9,7 +9,7 @@ object Regexp {
   val actorTok     = "actor".r
   val askTok       = "ask".r
   val assignTok    = "=".r
-  val atomTok      = """(#[A-Za-z_][\w'-]+)""".r // triple-quote ignores escapes and allows "
+  val atomTok      = """(#[A-Za-z_][\w'-]*)""".r // triple-quote ignores escapes and allows "
   val binLitTok    = "(0b[01]+)".r
   val commaTok     = ",".r
   val dataTok      = "data".r
@@ -17,7 +17,7 @@ object Regexp {
   val forTok       = "for".r
   val funcTok      = "func".r
   val hexLitTok    = "(0x[0-9A-Fa-f]+)".r
-  val idTok        = """([a-zA-Z_][\w'-]+)""".r
+  val idTok        = """([a-zA-Z_][\w'-]*)""".r
   val ifTok        = "if".r
   val importTok    = "import".r
   val inTok        = "in".r
