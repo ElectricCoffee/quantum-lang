1483bfdcb51efdddf6ec6e4f4622bbe6c24e157f
Niko Lepka <slench102+git@gmail.com>
2016-03-30 13:17:27 +0200

    add documentation

--- src/main/scala/dk/aau/sw404f16/util/Extensions.scala
+++ src/main/scala/dk/aau/sw404f16/util/Extensions.scala
@@ -1,7 +1,11 @@
 package dk.aau.sw404f16.util
 
-/**
-  * Created by coffee on 3/30/16.
+/** Created by coffee on 3/30/16.
+  * This file is for extension methods
+  * If you need to extend an existing class,
+  * create an "implicit class" in the Extensions object
+  * You can read more about implicit classes here:
+  * http://docs.scala-lang.org/overviews/core/implicit-classes.html
   */
 object Extensions {
   private val whitespace = "\\s".r // matches all whitespace, including spaces, tabs, and newlines
