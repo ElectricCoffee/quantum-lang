049c9c42470d6735a7bd4d6d31a30ab30e947f03
Niko Lepka <slench102+git@gmail.com>
2016-03-30 13:01:02 +0200

    remove oversights

--- src/main/scala/dk/aau/sw404f16/util/Extensions.scala
+++ src/main/scala/dk/aau/sw404f16/util/Extensions.scala
@@ -1,21 +1,11 @@
 package dk.aau.sw404f16.util
 
-import scala.annotation.tailrec
-
 /**
   * Created by coffee on 3/30/16.
-  * val head :: tail = input
-  * if(tail != Nil) head match {
-  * case " " | "" => stripSpaces(tail, acc)
-  * case _   => stripSpaces(tail, head :: acc)
-  * } else acc
   */
 object Extensions {
-  type StrLst = List[String] // type defines an alias
-  // implicit classes are how you add extension methods to classes
-  // they must exist within an object
-  implicit class RichStringList(val left: StrLst) extends AnyVal {
-    def stripSpaces: StrLst = left.filter {
+  implicit class RichStringList(val left: List[String]) extends AnyVal {
+    def stripSpaces: List[String] = left.filter {
       case " " | "" => false
       case _        => true
     }
