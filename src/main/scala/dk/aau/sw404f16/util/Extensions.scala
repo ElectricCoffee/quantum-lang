22fa9793d7f424456f0a9158a7a7913c1c7b213d
Niko Lepka <slench102+git@gmail.com>
2016-03-30 13:12:41 +0200

    add more general case for whitespace filtering

--- src/main/scala/dk/aau/sw404f16/util/Extensions.scala
+++ src/main/scala/dk/aau/sw404f16/util/Extensions.scala
@@ -4,10 +4,11 @@ package dk.aau.sw404f16.util
   * Created by coffee on 3/30/16.
   */
 object Extensions {
+  private val whitespace = "\\s".r // matches all whitespace, including spaces, tabs, and newlines
   implicit class RichStringList(val left: List[String]) extends AnyVal {
     def stripSpaces: List[String] = left.filter {
-      case " " | "" => false
-      case _        => true
+      case whitespace() | "" => false // exclude whitespace and empty strings
+      case _        => true  // include everything else
     }
   }
 }
