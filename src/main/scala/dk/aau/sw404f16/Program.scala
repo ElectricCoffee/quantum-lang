60d2ab2eb6494c792fc76a9bf3a32782e8659a0d
Niko Lepka <slench102+git@gmail.com>
2016-04-13 15:06:57 +0200

    add extension method for file loading

--- src/main/scala/dk/aau/sw404f16/Program.scala
+++ src/main/scala/dk/aau/sw404f16/Program.scala
@@ -2,43 +2,17 @@ package dk.aau.sw404f16
 
 import dk.aau.sw404f16.syntax.parser.Parser
 
-import scala.io.Source
+import dk.aau.sw404f16.util.Extensions.RichString
 
 /**
   * Created by coffee on 28/03/16.
   */
 object Program {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     println("It compiled!")
     // look in src/test/scala for the tests
-    val lines =
-      """module test;
-        |import Foo;
-        |receiver Program {
-        |  define Unit #main = {
-        |    val Num a = 3;
-        |    val b = "hello world";
-        |
-        |    if a > b then "hia";
-        |
-        |    for i in Bar do "hello";
-        |
-        |    for {
-        |     i in K;
-        |     j in F;
-        |    } yield {foo;};
-        |
-        |    match (foo) (Num bar) then stuff;
-        |    match (bar) {
-        |      "stuff" then stuff;
-        |      (Num baz) then morestuff;
-        |    };
-        |  };
-        |  define string #foo(bar) = "hello";
-        |  define num (string quux) = quux;
-        |}
-      """.stripMargin
+    val lines = "test.q".getFileContents.mkString
 
-    println(Parser.apply(lines))
+    println(Parser apply lines)
   }
 }
--- src/main/scala/dk/aau/sw404f16/util/Extensions.scala
+++ src/main/scala/dk/aau/sw404f16/util/Extensions.scala
@@ -1,5 +1,7 @@
 package dk.aau.sw404f16.util
 
+import scala.io.{Source, BufferedSource}
+
 /** Created by coffee on 3/30/16.
   * This file is for extension methods
   * If you need to extend an existing class,
@@ -9,6 +11,10 @@ package dk.aau.sw404f16.util
   */
 object Extensions {
   private val whitespace = "\\s".r // matches all whitespace, including spaces, tabs, and newlines
+  implicit class RichString(val left: String) extends AnyVal {
+    def getFileContents: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource(left).getPath)
+  }
+
   implicit class RichStringList(val left: List[String]) extends AnyVal {
     def stripSpaces: List[String] = left.filter {
       case whitespace() | "" => false // exclude whitespace and empty strings
