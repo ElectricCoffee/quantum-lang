d9dda5b29b399265820898b318377c2c98c0eaf9
Niko Lepka <slench102+git@gmail.com>
2016-04-19 14:40:46 +0200

    start match checker

--- src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ExpressionChecker.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ExpressionChecker.scala
@@ -2,20 +2,26 @@ package dk.aau.sw404f16.semantics.typeCheck
 
 import dk.aau.sw404f16.semantics.StandardType
 import dk.aau.sw404f16.semantics.exceptions.TypeMismatchException
-import dk.aau.sw404f16.syntax.{IfExpression, MatchExpression}
+import dk.aau.sw404f16.syntax.{IfExpression, MatchExpression, MatchStatement}
 
 /**
   * Created by coffee on 18/04/16.
   */
 object ExpressionChecker {
+  type TypeInfo = Option[(String, List[String])]
+  /**
+    * Checks an if-expression to see if all the types are valid
+    * the condition should be a boolean
+    * and all the return-expressions should return the same type
+    * @param ifExpr an IfExpression node in the tree
+    */
   def checkIf(ifExpr: IfExpression): Unit = {
     // check if all the ifs are booleans
-    val notBoolean = ifExpr
-      .statements
+    val notBoolean = ifExpr.statements
       .filter(_.boolean.concreteType contains StandardType.boolean)
 
     if(notBoolean.nonEmpty) {
-      val errMsg = notBoolean.map { expr =>
+      val errMsg: List[String] = notBoolean.map { expr =>
         val boolExpr = expr.boolean
         val linum = boolExpr.pos.line
         val conum = boolExpr.pos.column
@@ -36,6 +42,28 @@ object ExpressionChecker {
   }
 
   def checkMatch(matchExpr: MatchExpression): Unit = {
+    val referenceType = matchExpr.expression.concreteType
+    def exceptionHelper(matchStmts: List[MatchStatement]) = {
+      val errMsg = matchStmts.map { expr =>
+        val pattern    = expr.patternDefinition
+        val line       = pattern.pos.line
+        val column     = pattern.pos.column
+        val expression = pattern.pattern
+        s"The expression \"$expression\" on line $line, column $column does not match type $referenceType"
+      }
+      TypeMismatchException(errMsg mkString ", ")
+    }
 
+    // create a list of all the expressions that don't match the type
+    val mismatchReference = matchExpr.statements.filter(_.patternDefinition.concreteType != referenceType)
+    // if that list is empty, set the matchExpression's concrete type to referenceType
+    if (mismatchReference.nonEmpty) {
+      val mismatchSuper = matchExpr.statements.filter(_.patternDefinition.superType != referenceType)
+      if (mismatchSuper.nonEmpty) throw exceptionHelper(mismatchSuper)
+      else {
+        // do shit
+      }
+    }
+    else throw exceptionHelper(mismatchReference)
   }
 }
