3abab51c0dcf896be58cec81298ce4542e7fab6b
Niko Lepka <slench102+git@gmail.com>
2016-04-21 12:50:16 +0200

    add get-type
    helper function to get the type of an identifier

--- src/main/scala/dk/aau/sw404f16/semantics/SymbolTable.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/SymbolTable.scala
@@ -1,9 +1,24 @@
 package dk.aau.sw404f16.semantics
+import dk.aau.sw404f16.semantics.exceptions.NotYetDeclaredException
+import dk.aau.sw404f16.syntax.{ASTNode, Identifier}
+
+import scala.util.{Failure, Success, Try}
 import scala.collection.mutable
 /**
   * Created by coffee on 4/14/16.
   */
 class SymbolTable(scope: String) {
-  type SymTab = mutable.Map[String, Either[String, SymbolTable]]
+  type TypeInfo = Option[(String, List[String])]
+  type SymTab = mutable.Map[String, Either[(TypeInfo, ASTNode), SymbolTable]]
   var contents: SymTab = mutable.Map.empty
+
+  private def findValue(key: Identifier): Try[Either[(TypeInfo, ASTNode), SymbolTable]] = Try(contents(key.data))
+  private def noSuchIdentifier(name: String) =
+    NotYetDeclaredException("The identifier "+ name +" hasn't been declared")
+
+  /** gets the type of an identifier from the symbol table */
+  def getType(identifier: Identifier): TypeInfo = findValue(identifier) match {
+    case Success(v) => v.left.get._1
+    case Failure(_) => throw noSuchIdentifier(identifier.data)
+  }
 }
