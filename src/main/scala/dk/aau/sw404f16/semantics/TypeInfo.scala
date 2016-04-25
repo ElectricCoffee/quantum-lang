22d6f13562aaaced5818fbb3fd0b416dbd5dad9b
Niko Lepka <slench102+git@gmail.com>
2016-04-25 11:13:46 +0200

    fix type case

--- src/main/scala/dk/aau/sw404f16/semantics/TypeInfo.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/TypeInfo.scala
@@ -6,13 +6,13 @@ package dk.aau.sw404f16.semantics
 
 // "companion object" allows for static methods
 object TypeInfo {
-  val any     = new TypeInfo("any", Nil, null) // special case. Any is the equivalent of "Object" in C#
-  val number  = new TypeInfo("number")
-  val string  = new TypeInfo("string")
-  val boolean = new TypeInfo("boolean")
-  val unit    = new TypeInfo("unit") // equivalent to void
-  def list(valueType: TypeInfo) = new TypeInfo("list", List(valueType))
-  def dictionary(keyType: TypeInfo, valueType: TypeInfo) = ("dictionary", List(keyType, valueType))
+  val any     = new TypeInfo("Any", Nil, null) // special case. Any is the equivalent of "Object" in C#
+  val number  = new TypeInfo("Number")
+  val string  = new TypeInfo("String")
+  val boolean = new TypeInfo("Boolean")
+  val unit    = new TypeInfo("Unit") // equivalent to void
+  def list(valueType: TypeInfo) = new TypeInfo("List", List(valueType))
+  def dictionary(keyType: TypeInfo, valueType: TypeInfo) = ("Dictionary", List(keyType, valueType))
 }
 
 class TypeInfo(val concreteType: String, val typeArguments: List[TypeInfo], val superType: TypeInfo) {
