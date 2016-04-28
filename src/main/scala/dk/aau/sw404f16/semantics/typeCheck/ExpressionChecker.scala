7581b0c4a6dc9dcb935ae0afef25d6b11f94911b
electric coffee <slench102@gmail.com>
2016-04-28 17:04:50 +0200

    remove deprecated methods

--- src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ExpressionChecker.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ExpressionChecker.scala
@@ -66,36 +66,6 @@ object ExpressionChecker {
     }
   }
 
-  /**
-    * Checks an if-expression to see if all the types are valid
-    * the condition should be a boolean
-    * and all the return-expressions should return the same type
-    *
-    * @param ifExpr an IfExpression node in the tree
-    */
-  @deprecated def checkIf(ifExpr: IfExpression): Unit = {
-    // check if all the ifs are booleans
-    val notBoolean = ifExpr.statements // TODO: refactor later
-      .filter(_.boolean.nodeType <=> TypeInfo.boolean)
-
-    if(notBoolean.nonEmpty) {
-      val errMsg: List[String] = notBoolean.map { expr =>
-        s"expression ${lineRef(expr.boolean)} is not of type Bool"
-      }
-      throw TypeMismatchException(errMsg mkString ", ")
-    }
-
-    // check if all the expressions return the same type
-    // if they do, the overall type of the if expression is that type
-    val types = ifExpr.statements.map(_.body.nodeType)
-    val equal = types.forall(_ == types.head)
-
-    if(!equal)
-      throw TypeMismatchException("Not all paths return the same type, please make sure they do so.")
-    else
-      ifExpr.nodeType = types.head
-  }
-
   def checkMatchStmt(matchStmt: MatchStatement): (ASTNode, Expression) = matchStmt match {
     case MatchStatement(PatternDefinition(Left(literal)), expr) =>
       PatternChecker checkPattern literal
@@ -124,52 +94,6 @@ object ExpressionChecker {
 
     if (err.nonEmpty) throw TypeMismatchException(err)
     else retType
-  }
-
-  /** Checks match-expressions
-    * First makes sure that all the things checked on are
-    * either the same type as the input,
-    * or a subtype of the input.
-    * Then it checks to see if all the expressions are the same type also
-    *
-    * @param matchExpr an instance of a MatchExpression node
-    */
-  @deprecated def checkMatch(matchExpr: MatchExpression): Unit = {
-    val referenceType = matchExpr.expression.nodeType
-    def exceptionHelper(matchStmts: List[MatchStatement]) = {
-      val errMsg = matchStmts.map { expr =>
-        val pat = expr.patternDefinition
-        s"The pattern \"${pat.pattern}\" ${lineRef(pat)} does not match type $referenceType"
-      }
-      TypeMismatchException(errMsg mkString ", ")
-    }
-
-    // create a list of all the expressions that don't match the type
-    val mismatchReference = matchExpr.statements.filter(_.patternDefinition.nodeType <!=> referenceType)
-    // if that list is empty, set the matchExpression's concrete type to referenceType
-    if (mismatchReference.nonEmpty) {
-      val mismatchSuper = matchExpr.statements.filter(_.patternDefinition.nodeType <!^=> referenceType)
-      if (mismatchSuper.nonEmpty) throw exceptionHelper(mismatchSuper)
-      else { // if everything matches, check to see if all the expressions in the match return the same type
-        // TODO: Find a better way to do this so excessive iterations aren't used
-        // make sure all statements' types are evaluated
-        matchExpr.statements.foreach(x => checkExpression(x.body))
-        // set the first statement to be the baseline
-        val comparable = matchExpr.statements.head.body.nodeType
-        // filter out all statements that match, leaving behind all the mismatches
-        val mismatches = matchExpr.statements.filter(x => x.body.nodeType <!=> comparable)
-
-        if(mismatches.isEmpty) matchExpr.nodeType = comparable
-        else {
-          val err = mismatches.map { expr =>
-            val pos = expr.body.pos
-            s"The expression on line ${pos.line}, column ${pos.column}, doesn't match type $comparable"
-          }
-          throw TypeMismatchException(err mkString ", ")
-        }
-      }
-    }
-    else throw exceptionHelper(mismatchReference)
   }
 
   def checkForStmt(forStmt: ForStatement): Identifier = forStmt match {
