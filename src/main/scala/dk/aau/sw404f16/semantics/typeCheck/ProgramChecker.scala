a6ebaecb56d9025950a1ef78320f79678b37bc9f
Niko Lepka <slench102+git@gmail.com>
2016-05-02 13:32:28 +0200

    add check-actor

--- src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ProgramChecker.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/typeCheck/ProgramChecker.scala
@@ -1,5 +1,6 @@
 package dk.aau.sw404f16.semantics.typeCheck
 
+import dk.aau.sw404f16.semantics.TypeInfo
 import dk.aau.sw404f16.semantics.exceptions.TypeMismatchException
 import dk.aau.sw404f16.syntax._
 import dk.aau.sw404f16.util.Convenience.lineRef
@@ -10,13 +11,25 @@ import dk.aau.sw404f16.util.Convenience.lineRef
 object ProgramChecker {
   def checkProgram(program: Program) = program.constructors.map {
     // name @ Pattern is a way to bind a name to a pattern
-    case module @ ModuleImport(moduleName) => ???
-    case receiver @ ReceiverDefinition(primary, inherited, body) => ???
-    case actor @ ActorDefinition(primary, inherited, body) => ???
+    case module @ ModuleImport(moduleName) => ??? // TODO: Define module in symbol table
+    case ReceiverDefinition(primary, inherited, body) => checkActorDef(primary, inherited, body)
+    case ActorDefinition(primary, inherited, body)    => checkActorDef(primary, inherited, body)
     case struct @ DataStructureDefinition(typeDef, data, inherited) => ???
   }
 
-  def checkActorBody(body: ActorBodyBlock) = ???
+  def checkActorDef(primTyp: TypeDefinition, inhTyp: Option[List[TypeDefinition]], body: ActorBodyBlock): TypeInfo = {
+    checkActorBody(body) // TODO: store and define all the message types in the symbol table
+    val primary = primTyp.toTypeInfo
+    inhTyp match {
+      case Some(inhs) =>
+        val supers = inhs.map(_.toTypeInfo)
+        primary makeSubTypeOf supers
+      case None       => primary
+    }
+    // TODO: store this information in the symbol table
+  }
+
+  def checkActorBody(body: ActorBodyBlock): Unit = ???
 
   def checkMsgDef(msgDef: MessageDefinition) = msgDef match {
     case MessageDefinition(typeDef, pattern, block) =>
