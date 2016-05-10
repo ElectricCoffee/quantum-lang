31b820aa9595d447000152e57453c0c41ee5b297
electric coffee <slench102@gmail.com>
2016-05-10 23:11:03 +0200

    create symbol-table-populator

---
+++ src/main/scala/dk/aau/sw404f16/semantics/SymbolTablePopulator.scala
@@ -0,0 +1,9 @@
+package dk.aau.sw404f16.semantics
+
+/**
+  * Created by coffee on 10/05/16.
+  */
+// I have a vague feeling it might be easier to traverse the whole thing after the AST's made
+object SymbolTablePopulator {
+
+}
