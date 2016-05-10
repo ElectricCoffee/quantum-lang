0b5a39b513bd189aa271a4118de9e5ee0339ff1c
Niko Lepka <slench102+git@gmail.com>
2016-05-10 12:50:27 +0200

    update atom-table

--- src/main/scala/dk/aau/sw404f16/semantics/AtomTable.scala
+++ src/main/scala/dk/aau/sw404f16/semantics/AtomTable.scala
@@ -1,6 +1,7 @@
 package dk.aau.sw404f16.semantics
 
-import dk.aau.sw404f16.syntax.Expression
+import com.sun.jndi.toolkit.ctx.AtomicDirContext
+import dk.aau.sw404f16.syntax.{Atom, Expression}
 
 import scala.collection.mutable
 
@@ -8,5 +9,19 @@ import scala.collection.mutable
   * Created by coffee on 5/9/16.
   */
 object AtomTable {
-  val table = mutable.Map[String, List[Expression]]
+  private type AtomDict = mutable.Map[String, List[Expression]]
+  val table: AtomDict = mutable.Map.empty
+
+  def addAtom(atom: String, args: List[Expression]): Unit = {
+    if (!table.contains(atom))
+      table += atom -> args
+
+    // TODO: decide what this method should return
+  }
+
+  def addAtom(atom: Atom, args: List[Expression]): Unit = addAtom(atom.data, args)
+
+  def addAtom(atom: String): Unit = addAtom(atom, Nil)
+
+  def addAtom(atom: Atom): Unit = addAtom(atom, Nil)
 }
