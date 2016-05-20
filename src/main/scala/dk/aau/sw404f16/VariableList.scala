ce4e25b58afc17429beef357f97711c37cf9cf5d
Niko Lepka <slench102+git@gmail.com>
2016-05-20 09:59:28 +0200

    Change VariableList to a vector for better performance

--- src/main/scala/dk/aau/sw404f16/VariableList.scala
+++ src/main/scala/dk/aau/sw404f16/VariableList.scala
@@ -6,12 +6,12 @@ package dk.aau.sw404f16
 object VariableList {
   val root = new VariableList(null)
 }
+// uses Vector instead of List due to quicker search time
+class VariableList(val parent: VariableList, private var collection: Vector[Either[String, VariableList]] = Vector()) {
+  def this(parent: VariableList) = this(parent, Vector())
 
-class VariableList(val parent: VariableList, private var collection: List[Either[String, VariableList]] = Nil) {
-  def this(parent: VariableList) = this(parent, Nil)
-
-  def addVariable(variable: String) = collection = Left(variable) :: collection
-  def addScope(scope: VariableList) = collection = Right(new VariableList(this)) :: collection
+  def addVariable(variable: String) = collection = Left(variable) +: collection
+  def addScope(scope: VariableList) = collection = Right(new VariableList(this)) +: collection
 
   def currentScope: VariableList = collection.head match {
     case Left(_) => this
