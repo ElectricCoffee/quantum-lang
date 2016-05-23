011ad3fa16971e79a28f82402f3b83c43e8ec0af
Niko Lepka <slench102+git@gmail.com>
2016-05-23 14:18:59 +0200

    change scope to accomodate for associated records

--- src/main/scala/dk/aau/sw404f16/VariableList.scala
+++ src/main/scala/dk/aau/sw404f16/VariableList.scala
@@ -1,4 +1,5 @@
 package dk.aau.sw404f16
+import scala.collection.mutable
 
 /**
   * Created by coffee on 5/19/16.
@@ -7,8 +8,8 @@ object VariableList {
   private val scope = scala.collection.mutable.Stack(new Scope(null))
 
   def currentScope: Scope = scope.head.collection.head match {
-    case Right(innerScope) => innerScope
-    case _                 => scope.head
+    case (Right(innerScope), _) => innerScope
+    case _                      => scope.head
   }
 
   def contains(name: String): Boolean = currentScope.collection contains Left(name)
@@ -26,18 +27,43 @@ object VariableList {
     currentScope
   }
 
+  def hasAssociatedRecord(name: String) = {
+    if (!contains(name))
+      throw new IllegalArgumentException(s"the value $name hasn't been defined")
+
+    currentScope.collection(Left(name)).isDefined // returns true if there is an associated record
+  }
+
+  def getAssociatedRecord(name: String): String = {
+    if (hasAssociatedRecord(name))
+      currentScope.collection(Left(name)).get
+    else
+      throw new IllegalArgumentException(s"the value $name doesn't have an associated record")
+  }
+
+  def getAssociatedRecord(name: String, default: String): String = try {
+    getAssociatedRecord(name)
+  } catch {
+    case iae: IllegalArgumentException => default
+  }
+
   def getScope   = scope.head
   def leaveScope = scope.pop
 }
 
-class Scope(val parent: Scope, var collection: Vector[Either[String, Scope]] = Vector()) {
+class Scope(val parent: Scope, val collection: mutable.Map[Either[String, Scope], Option[String]] = mutable.Map.empty) {
   def addValue(name: String): Scope = {
-    collection = Left(name) +: collection
+    collection += Left(name) -> None
+    this
+  }
+
+  def addValue(name: String, struct: String): Scope = {
+    collection += Left(name) -> Some(struct)
     this
   }
 
   def addScope(newScope: Scope): Scope = {
-    collection = Right(newScope) +: collection
+    collection += Right(newScope) -> None
     newScope
   }
 }
